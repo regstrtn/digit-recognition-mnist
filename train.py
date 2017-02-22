095d7e0e6e4f9b9d0d5b11bbc4bbe4a157718a0b
regstrtn <regstrtn@gmail.com>
2017-02-22 14:22:31 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,36 @@
+'''
+Deep Learning Programming Assignment 1
+--------------------------------------
+Name: Mohammad Luqman
+Roll No.:   16CS60R52
+
+======================================
+Complete the functions in this file.
+Note: Do not change the function signatures of the train
+and test functions
+'''
+import numpy as np
+
+class network(object):
+    def __init__(self, layers, indim, hdim, outdim):
+        self.b = 
+
+
+def train(trainX, trainY):
+    '''
+    Complete this function.
+    '''
+
+
+def test(testX):
+    '''
+    Complete this function.
+    This function must read the weight files and
+    return the predicted labels.
+    The returned object must be a 1-dimensional numpy array of
+    length equal to the number of examples. The i-th element
+    of the array should contain the label of the i-th test
+    example.
+    '''
+
+    return np.zeros(testX.shape[0])
